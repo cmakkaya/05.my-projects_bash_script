1a79ac9795cd53047eedfa7a129105fdffcf27af
cmakkaya <cmakkaya.aws@gmail.com>
2022-09-01 08:24:23 +0300

    favori araba seçimi script

---
+++ favorite_car_read-p.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter your three favorite car: " CAR1 CAR2 CAR3
+
+echo "Your first favorite car is $CAR1"
+
+echo "Your second favorite car is $CAR2"
+
+echo "Your third favorite car is $CAR3"
